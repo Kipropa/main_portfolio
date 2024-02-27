4b4de60124229ed641972364537b94bb4fd56d1a
Kiprop <kipropa77@gmail.com>
2024-02-27 13:24:14 +0300

    hto

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,6 +13,7 @@ export default function Home() {
       <AboutSection />
       <ServicesSection />
       <ProjectsSection />
+      <TestimonialsSection />
       <ContactBanner />
     </div>
   );
